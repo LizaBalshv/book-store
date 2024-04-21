ec120f49f357d897f6e7f7624c9a2903a6bbcf14
Liza <lizabalashova7@gmail.com>
2024-04-21 15:38:25 +0300

    fixed style

--- src/main/java/com/example/bookstore/service/BookServiceImpl.java
+++ src/main/java/com/example/bookstore/service/BookServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -34,6 +35,23 @@ public class BookServiceImpl implements BookService {
         Book book = bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Can't find book by id " + id)
         );
+        return bookMapper.toDto(book);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public BookDto update(Long id, CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        book.setId(id);
+        if (bookRepository.existsById(book.getId())) {
+            bookRepository.save(book);
+        } else {
+            throw new NoSuchElementException("Can't update book: " + book);
+        }
         return bookMapper.toDto(book);
     }
 }
